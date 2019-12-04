3d5654861aa52e7d94758e0434fb29918bcf069c
CDearman452 <58072257+CDearman452@users.noreply.github.com>
2019-12-04 13:46:10 +0000

    Update-Names

--- MS5403-2DProject01/Assets/CDearmanMorecraft/Scripts/CDM_TopDownReticule.cs
+++ MS5403-2DProject01/Assets/CDearmanMorecraft/Scripts/CDM_TopDownReticule.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class CDM_TopDownReticule : MonoBehaviour
 {
+    //----------------------------------------------------------------------------------------------------
+    // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false; // Make the cursor invisible
+        Cursor.lockState = CursorLockMode.Confined; // Lock the cursor to the game window
     }
+    //----------------------------------------------------------------------------------------------------
     // Update is called once per frame
     void Update()
     {
+        // Check the mouses current position in the camera, convert to world space and store as a vector
         Vector2 l_v2_MousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        if (transform.position != new Vector3(l_v2_MousePos.x, l_v2_MousePos.y, -1))
-        {
-            Vector2 l_v2_PosDif = l_v2_MousePos - new Vector2(transform.position.x, transform.position.y);
-            GetComponent<Rigidbody2D>().velocity = l_v2_PosDif;
-        }
-        else GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        transform.position = new Vector3(l_v2_MousePos.x, l_v2_MousePos.y, -1); // Update Reticule Objects position to match the cursors
     }
 }
